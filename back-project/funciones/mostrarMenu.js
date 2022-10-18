e2fe7a38eb4b88d83a21ee5422d21d9bdb96e71d
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-10-18 18:56:45 -0500

    se agrego boton de ocultar menu

---
+++ back-project/funciones/mostrarMenu.js
@@ -0,0 +1,24 @@
+const mostrarMenu= ()=>{
+    const barraNavegacion = document.getElementById('barraNavegacion')
+    barraNavegacion.hidden = false;
+}
+const ocultarMenu = ()=>{
+    barraNavegacion.hidden = true;  
+}
+
+const mostrarMenu1= ()=>{
+    const barraNavegacion = document.getElementById('barraNave')
+    barraNave.hidden = false;
+}
+const ocultarMenu1 = ()=>{
+    barraNave.hidden = true;  
+}
+
+
+const mostrarMenu2= ()=>{
+    const barraNavegacion = document.getElementById('barraNave2')
+    barraNave2.hidden = false;
+}
+const ocultarMenu2 = ()=>{
+    barraNave2.hidden = true;  
+}
