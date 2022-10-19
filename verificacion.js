c32355ee76f689d79a0c3e1b086fc3d0a68ea280
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2022-10-19 00:11:00 -0500

    se crea registro y se personaliza

--- verificacion.js
+++ verificacion.js
@@ -1,8 +1,47 @@
-const verificar= ()=>{
+const verificar= async()=>{
     const inputusuario= document.getElementById('usuario').value;
     const inputClve= document.getElementById('clave').value;
         if(inputusuario =="Kuepa2022" && inputClve =="kuepa2022*" ){
-            window.location ='/back-project/administrador.html';
+            const { value: accept } = await Swal.fire({
+                title: 'Terminos y Condiciones',
+                input: 'checkbox',
+                inputValue: 1,
+                inputPlaceholder:
+                  'De acuerdo con nuestros T&C',
+                confirmButtonText:
+                  'Continue <i class="fa fa-arrow-right"></i>',
+                inputValidator: (result) => {
+                  return !result && 'es necesario que aceptes nuestros T&C'
+                }
+              })
+              
+              if (accept) {
+                Swal.fire(window.location ='/back-project/administrador.html');
+              };
         
+
+
         }else(Swal.fire("clave incorrecta"))
+
+
+    }
+    const  register= async()=>{
+
+
+        const { value: accept } = await Swal.fire({
+            title: 'Terminos y Condiciones',
+            input: 'checkbox',
+            inputValue: 1,
+            inputPlaceholder:
+              'De acuerdo con nuestros T&C',
+            confirmButtonText:
+              'Continue <i class="fa fa-arrow-right"></i>',
+            inputValidator: (result) => {
+              return !result && 'es necesario que aceptes nuestros T&C'
+            }
+          })
+          
+          if (accept) {
+            Swal.fire(window.location ='/back-project/index.html');
+          }
     }
